26ce6fef0bce6e3fb05b2c3581edb3d71f5b49ee
Chuz777 <72413524+Chuz777@users.noreply.github.com>
2024-08-08 20:34:48 -0600

    avance
    avance

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -4,23 +4,28 @@ import lombok.Data;
 import java.io.Serializable;
 import jakarta.persistence.*;
 
-
 @Data
 @Entity
-@Table(name="categoria")
-public class Categoria {
-    
-    private static final long serialVersionUID= 1L;
-    
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name="id_categoria")
-    
-    
-    
-  private Long id_categoria;
-  private String descripcion;
-  private String ruta_imagen;
-  private boolean activo;
-    
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+
+    private Long idCategoria;
+    private String descripcion;
+    private String ruta_imagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String categoria, boolean activo) {
+        this.descripcion = categoria;
+        this.activo = activo;
+    }
+
 }
